5dade54fcbd7eb7c5a0d62f73e6e7dc41a5a9ab1
Nick Beckwith <nickbeck@seas.upenn.edu>
2021-04-24 18:56:20 -0400

    Create wsl_setup.ps1

---
+++ wsl_setup.ps1
@@ -0,0 +1,8 @@
+echo "Setting up WSL with Yubikey..."
+wsl sudo apt install -y socat
+wsl mkdir ~/.ssh
+wsl wget https://github.com/BlackReloaded/wsl2-ssh-pageant/releases/download/v1.2.0/wsl2-ssh-pageant.exe -O ~/.ssh/wsl2-ssh-pageant.exe
+wsl chmod +x ~/.ssh/wsl2-ssh-pageant.exe
+# Add SSH to dotfile
+wsl cp 50-sshgpg-socket ~/.zshrc.d/
+wsl.exe --shutdown
