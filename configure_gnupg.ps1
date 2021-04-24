73672f749d73cd04169f1951188e110947676195
Nick Beckwith <nickbeck@seas.upenn.edu>
2021-04-24 17:58:08 -0400

    Adds verification

--- configure_gnupg.ps1
+++ configure_gnupg.ps1
@@ -1,2 +1,7 @@
 mkdir %HOMEPATH%\AppData\Roaming\gnupg
 echo enable-putty-support◙enable-ssh-support > %HOMEPATH%\AppData\Roaming\gnupg\gpg-agent.conf
+echo "If configured properly, card status should be shown below"
+echo "---------------------------------------------------------"
+gpg --card-status
+echo "---------------------------------------------------------"
+echo "If not configured properly, visit https://dev.to/dzerycz/the-ultimate-guide-to-yubikey-on-wsl2-part-1-5aed"
