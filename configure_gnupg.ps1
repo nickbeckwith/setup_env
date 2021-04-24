50081ec330f51cd71449b9478defb0dc99780a41
Nick Beckwith <nickbeck@seas.upenn.edu>
2021-04-24 18:04:03 -0400

    Adds error checking

--- configure_gnupg.ps1
+++ configure_gnupg.ps1
@@ -3,5 +3,7 @@ echo enable-putty-support◙enable-ssh-support > %HOMEPATH%\AppData\Roaming\gnup
 echo "If configured properly, card status should be shown below"
 echo "---------------------------------------------------------"
 gpg --card-status
-echo "---------------------------------------------------------"
-echo "If not configured properly, visit https://dev.to/dzerycz/the-ultimate-guide-to-yubikey-on-wsl2-part-1-5aed"
+if ($LASTEXITCODE -ne "0") {
+  echo "GnuPG is not configured properly. Visit https://dev.to/dzerycz/the-ultimate-guide-to-yubikey-on-wsl2-part-1-5aed"
+  exit 1
+}
