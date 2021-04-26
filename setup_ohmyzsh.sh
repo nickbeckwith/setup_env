7aba58cfa34a86d93bbf912c799c17567cd2b71a
Nick Beckwith <nickbeck@seas.upenn.edu>
2021-04-26 11:47:46 -0400

    Create setup_ohmyzsh.sh

---
+++ setup_ohmyzsh.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e 
+
+# install zshrc
+sudo apt install -Y zsh
+sudo sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+source ~/.zshrc
+
+# Install "neccesary" plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# Copy .zshrc from repo to home directory
+cp .zshrc ~/.zshrc
