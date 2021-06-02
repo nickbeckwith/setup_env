683b97222b88f0f1592d7aa3fe018bc8006d80bf
Nick Beckwith <nickbeck@seas.upenn.edu>
2021-06-02 13:02:35 -0400

    Update setup_ohmyzsh.sh

--- setup_ohmyzsh.sh
+++ setup_ohmyzsh.sh
@@ -2,13 +2,16 @@
 set -e 
 
 # install zshrc
-sudo apt install -Y zsh
+sudo apt install -y zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-source ~/.zshrc
 
 # Install "neccesary" plugins
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 # Copy .zshrc from repo to home directory
 cp .zshrc ~/.zshrc
+
+# Change shell to zsh
+sudo chsh $USER -s /usr/bin/zsh
