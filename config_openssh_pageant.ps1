e975e1db9a902ca04f6bc2068faf5b74a6e7370c
Nick Beckwith <nickbeck@seas.upenn.edu>
2021-06-04 13:23:46 -0400

    Update config_openssh_pageant.ps1

--- config_openssh_pageant.ps1
+++ config_openssh_pageant.ps1
@@ -5,5 +5,5 @@ wsl wget https://github.com/benpye/wsl-ssh-pageant/releases/download/20201121.2/
 New-Item -ItemType SymbolicLink -Path "C:\Users\nick\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\gpg-agent.exe" -Target "C:\Program Files (x86)\gnupg\bin\gpg-agent.exe"
 gpg-agent.exe
 # Create startup script for wsl-ssh-pageant
-Set-Content "C:\Users\nick\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\start_ssh_pageant.bat" "start "wsl-ssh-pageant-amd64.exe" "--winssh ssh-pageant --systray""
-Set-Content "C:\Users\nick\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\start_ssh_pageant.bat" 'start "wsl-ssh-pageant-amd64.exe" "--winssh ssh-pageant --systray"'
+Set-Content "C:\Users\nick\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\start_ssh_pageant.bat" 'start "wsl-ssh-pageant-amd64.exe --winssh ssh-pageant --systray"
+'
