7e0d77f87369ec7221ad333dd284f45f0928ed9b
Nick Beckwith <nickbeck@seas.upenn.edu>
2021-06-04 12:04:57 -0400

    Update wsl_yubikey_setup.ps1

--- wsl_yubikey_setup.ps1
+++ wsl_yubikey_setup.ps1
@@ -1,6 +1,6 @@
 echo "Setting up WSL with Yubikey..."
 wsl sudo apt install -y socat
-wsl mkdir ~/.ssh
+wsl mkdir -p ~/.ssh
 wsl wget https://github.com/BlackReloaded/wsl2-ssh-pageant/releases/download/v1.2.0/wsl2-ssh-pageant.exe -O ~/.ssh/wsl2-ssh-pageant.exe
 wsl chmod +x ~/.ssh/wsl2-ssh-pageant.exe
 # Add SSH to dotfile
