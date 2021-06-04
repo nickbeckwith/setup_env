e66581eddd6ea92b8c42fa2ca2102276f9076441
Nick Beckwith <nickbeck@seas.upenn.edu>
2021-06-04 12:35:29 -0400

    Update wsl_yubikey_setup.ps1

--- wsl_yubikey_setup.ps1
+++ wsl_yubikey_setup.ps1
@@ -4,5 +4,5 @@ wsl mkdir -p ~/.ssh
 wsl wget https://github.com/BlackReloaded/wsl2-ssh-pageant/releases/download/v1.2.0/wsl2-ssh-pageant.exe -O ~/.ssh/wsl2-ssh-pageant.exe
 wsl chmod +x ~/.ssh/wsl2-ssh-pageant.exe
 # Add SSH to dotfile
-wsl cp 50-sshgpg-socket ~/.zshrc.d/
+wsl cp 50-sshgpg-socket.zsh ~/.zshrc.d/
 wsl.exe --shutdown
