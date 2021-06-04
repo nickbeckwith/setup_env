ed5fe42267f2071b65c147adf53be61aa0928192
Nicholas Beckwith <nickbeck@seas.upenn.edu>
2021-06-04 12:02:23 -0400

    Adds dot dirs

--- setup_ohmyzsh.sh
+++ setup_ohmyzsh.sh
@@ -9,6 +9,7 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone https://github.com/mattmc3/zshrc.d $ZSH_CUSTOM/plugins/zshrc.d
 
 # Copy .zshrc and p10k configuration from repo to home directory
 cp .zshrc ~/.zshrc
